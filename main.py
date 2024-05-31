6331543af65fca76a9f2f3345c87e777f0aec1e1
Chris Cooley <ccooley@bonadmin.com>
2024-05-31 10:35:17 -0600

    Add Post operations

--- main.py
+++ main.py
@@ -22,35 +22,35 @@ for piece_data in pieces_list:
 
 
 @app.get("/composers")
-def get_composers() -> list[Composer]:
+async def get_composers() -> list[Composer]:
     return composers
 
 @app.get("/pieces")
-def get_pieces(composer_id: Optional[int] = None) -> list[Piece]:
+async def get_pieces(composer_id: Optional[int] = None) -> list[Piece]:
     if composer_id is not None:
         return [piece for piece in pieces if piece.composer_id == composer_id]
     return pieces
 
 @app.post("/composers")
-def create_composer(composer: Composer):
-    pass
+async def create_composer(composer: Composer) -> None:
+    composers.append(composer)
 
 @app.post("/pieces")
-def create_piece(piece: Piece):
-    pass
+async def create_piece(piece: Piece) -> None:
+    pieces.append(piece)
 
 @app.put("/composers/{composer_id}")
-def update_composer(composer_id: int, composer: Composer):
+async def update_composer(composer_id: int, composer: Composer):
     pass
 
 @app.put("/pieces/{piece_name}")
-def update_piece(piece_name: str, piece: Piece):
+async def update_piece(piece_name: str, piece: Piece):
     pass
 
 @app.delete("/composers/{composer_id}")
-def delete_composer(composer_id: int):
+async def delete_composer(composer_id: int):
     pass
 
 @app.delete("/pieces/{piece_name}")
-def delete_piece(piece_name: str):
+async def delete_piece(piece_name: str):
     pass
